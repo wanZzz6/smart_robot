09dbb37fd16c5d43b733ed25956473eb5da42ce8
wanzheng <1343837706@qq.com>
2018-07-15 12:25:03 +0800

    配置文件

---
+++ config/config.py
@@ -0,0 +1,15 @@
+# 音乐播放器运行文件（ 我的是网易云）
+music_exe = r'E:\软件安装\CloudMusic\cloudmusic.exe'
+
+# 本地音乐库，音乐库中没有的 歌曲自动打开百度搜索曲目，
+# 也可以自己写爬虫去各大音乐网站下载到本地，然后播放，或者直接返回到前端
+music_dir = 'D:\网易云音乐'
+
+# 截图保存路径，放桌面比较方便
+capture_save_path = r'C:\Users\wanzheng\Desktop'
+
+#音频角色，音量，语速调节 参照百度云服务 文档
+#https://cloud.baidu.com/doc/SPEECH/TTS-Online-Python-SDK.html#.E5.BF.AB.E9.80.9F.E5.85.A5.E9.97.A8
+role_param ={'vol': '7', 'pit': '5', 'per': '0'}
+
+
