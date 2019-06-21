66fe73b125eae2be994e4e96ea07c48fce2a0076
wanZzz <1343837706@qq.com>
2019-06-21 16:49:49 +0800

    添加历史记录

--- audio_bp/audio_recognize.py
+++ audio_bp/audio_recognize.py
@@ -1,9 +1,12 @@
+import datetime
+
 from flask import Blueprint
 from flask import request
 from flask import jsonify
 from Lsi_gensim import get_high_sim, index
 from robot_function.tuling import chat_robot
 from baidu_ai import baidu_API
+import time
 
 au_bp = Blueprint("a", __name__, static_folder='saudio_template')
 
@@ -26,6 +29,9 @@ def upload():
     other_info = ''
 
     if not audio_text.startswith('Sorry'):
+        with open('../static/history.txt', 'a+', encoding='utf-8') as f:
+            timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+            f.write(timestamp+' 我>>>'+audio_text+'\n')
         doc_index = get_high_sim(audio_text, index)
         # print(doc_index,'-----------')
         if doc_index is not None:
@@ -49,6 +55,9 @@ def upload():
                 answer = baidu_API.nlp_simnet(doc_index)
         else:
             answer = chat_robot(audio_text)
+            with open('../static/history.txt', 'a+', encoding='utf-8') as f:
+                timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+                f.write(timestamp + ' 机器人>>>' + audio_text + '\n')
             print('机器人回答:', answer)
     else:
         answer = audio_text
